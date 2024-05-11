4514b31f123203570c80986fd65f93d7ab917e19
rosmarenriquez <rosmar.em@outlook.es>
2024-05-11 12:17:47 -0600

    changes done

--- scripts/place.js
+++ scripts/place.js
@@ -12,3 +12,19 @@ if (temperature <= 10 && windSpeed > 4.8) {
 } else{
     windChillElement.textContent = "N/A";
 }
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
